e5fd643a6023471add6d15027f429e8b45c6684b
Daffa Dappo <daffa20dappo@gmail.com>
2021-12-16 19:25:45 +0700

    Sensor-Accelerometer App

---
+++ app/src/main/java/com/example/sensoraccelerometer/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.sensoraccelerometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private TextView xText, yText, zText;
+    private Sensor mySensor;
+    private SensorManager SM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        xText = (TextView)findViewById(R.id.xText);
+        yText = (TextView)findViewById(R.id.yText);
+        zText = (TextView)findViewById(R.id.zText);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xText.setText("X : " + event.values[0]);
+        yText.setText("Y : " + event.values[1]);
+        zText.setText("Z : " + event.values[2]);
+    }
+}
